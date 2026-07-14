8ef2991011d71e752d3291c7da4f60dcec3780bd
zainabtcs27-crypto <zainabt.cs27@gmail.com>
2026-07-14 19:52:19 +0500

    1st mini project

---
+++ Number_Guessing_Game.py
@@ -0,0 +1,25 @@
+import random
+
+random_number = (random.randint(1,100))
+count = 0
+
+while True:   
+   num = int(input("Please enter your input to guess a num: ")) 
+   count +=1
+   if count==7:
+      print(f"You have used up all your attempts.\nYou had to guess the number {random_number}.")
+      break
+   if random_number < num:
+      print(f"Your guessed number {num} is too high.")
+   elif random_number > num:
+      print(f"Your guessed number {num} is too low.")
+   elif random_number==num:
+      print(f"You have guessed a correct number {num}")
+      break
+
+print(f"You have done {count} attempts to guess a number.")
+
+
+
+# print(f"random genereted number is {random_number}")
+
